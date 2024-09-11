b879a323a4b9d5585bf8c2d0e5d41313ede19901
Chaninan Phetpangun <chaninan.phe@student.mahidol.ac.th>
2024-09-11 20:08:26 +0700

    update test

--- src/test/java/RationalTest.java
+++ src/test/java/RationalTest.java
@@ -34,8 +34,66 @@ public class RationalTest {
         Assert.assertEquals(4, r1.denominator);
     }
 
+    @Test
+    public void testSubtract() {
+        r1.numerator = 3;
+        r1.denominator = 4;
+        r2.numerator = 1;
+        r2.denominator = 4;
+        r1.subtract(r2);
+        Assert.assertEquals(1, r1.numerator);
+        Assert.assertEquals(2, r1.denominator);
+    }
+
+    @Test
+    public void testMultiply() {
+        r1.numerator = 3;
+        r1.denominator = 4;
+        r2.numerator = 2;
+        r2.denominator = 3;
+        r1.multiply(r2);
+        Assert.assertEquals(1, r1.numerator); // Er1pected result after simplification
+        Assert.assertEquals(2, r1.denominator);
+    }
+
+    @Test
+    public void testDivide() {
+        r1.numerator = 3;
+        r1.denominator = 4;
+        r2.numerator = 2;
+        r2.denominator = 3;
+        r1.divide(r2);
+        Assert.assertEquals(9, r1.numerator); // 3/4 รท 2/3 = 3/4 * 3/2 = 9/8
+        Assert.assertEquals(8, r1.denominator);
+    }
+
+    @Test
+    public void testEquals() {
+        r1.numerator = 3;
+        r1.denominator = 4;
+        r2.numerator = 6;
+        r2.denominator = 8; // Equivalent to 3/4 after simplification
+        Assert.assertTrue(r1.equals(r2));
+    }
+
+    @Test
+    public void testCompareTo() {
+        r1.numerator = 1;
+        r1.denominator = 2;
+        r2.numerator = 3;
+        r2.denominator = 4;
+        Assert.assertEquals(-1, r1.compareTo(r2)); // r1 is less than r2
+    }
+
+    @Test
+    public void testToString() {
+        r1.numerator = 3;
+        r1.denominator = 4;
+        Assert.assertEquals("3/4", r1.toString());
+    }
+
     /***
-     * This method will be called every time after the other @Test method
+     * This method will be called everr2 time after the other @Test method
      * is called.
      */
     @After
